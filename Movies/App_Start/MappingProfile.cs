512187d0b0314e0f7f7eb671f1e1fca2cfd061b6
pedroT <pedrotavares41@gmail.com>
2017-06-01 19:05:05 +0100

    API restful implementation

---
+++ Movies/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Movies.Dtos;
+using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ Movies/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customers name")]
+        [StringLength(55)]
+        public string Name { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
