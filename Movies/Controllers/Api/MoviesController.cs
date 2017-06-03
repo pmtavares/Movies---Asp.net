f8e5667667c14a11e9a0183a2363d970f1f0ad80
pedroT <pedrotavares41@gmail.com>
2017-06-03 15:39:33 +0100

    MoviesDelete and CustomersDataTable

---
+++ Movies/Controllers/Api/MoviesController.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using Movies.Dtos;
+using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movies.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        //Get /Api/movies
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+
+        //Get /Api/movies/id
+        
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+            
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+
+
+        }
+
+
+        //Put /Api/movies/id
+        [HttpPut]
+        public void updateMovie(int id, MovieDto movieDto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            Mapper.Map(movieDto, movie);
+            
+            _context.SaveChanges();
+
+        }
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Movies.Remove(movie);
+
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ Movies/Dtos/MovieDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Dtos
+{
+    public class MovieDto
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Release date")]
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in stock")]
+        [Required]
+        [Range(1, 20, ErrorMessage = "The number must be between 1 and 20")]
+        public int NumberInStock { get; set; }
+
+        //public DateTime? DataAdd { get; set; }
+
+        [Display(Name = "Genre")]
+         
+        [Required]
+        public byte MovieGenreId { get; set; }
+    }
+}
